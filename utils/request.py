58baa6e249bb9b997d7343a4231c7f317d93e97b
zhangyingkai <zyk123456789>
2021-07-28 10:28:19 +0800

    修改request工具类

--- utils/request.py
+++ utils/request.py
@@ -1,20 +1,27 @@
 import requests
-from loguru import logger
-import time
 import jsonpath
-import re
 import json
+import re
+from loguru import logger
 
 
-class RequestUtil(requests.Session):
+class RequestUtil(requests.Session):  # 可以自动缓存Cookie
 
     def __init__(self):
         super().__init__()
-        self.variable_pool = {}  # 定义变量池
-        self.retry_num = 0  # 定义初始次数
+        self.variable_pool = {}  # 全局变量池
 
     def send_request(self, method, url, title, jsonpath_exp_save=None, regular_exp_save=None,
                      jsonpath_exp_assertion=None, regular_exp_assertion=None, **kwargs):
+        """
+        发送请求
+        需求：1.调接口，get/post/put/delete
+            2.需要打印相关日志
+            3.异常处理
+            4.重试机制
+            5.超时...
+        :return:
+        """
         logger.info(f"CASE {title} START".center(40, "*"))
         logger.info(f"接口请求方式：{method}")
         logger.info(f"接口url：{self.replace_template_str(url)}")
@@ -22,104 +29,113 @@ class RequestUtil(requests.Session):
         logger.info(f"正则提取表达式： {regular_exp_save}")
         logger.info(f"JSONPATH断言表达式： {jsonpath_exp_assertion}")
         logger.info(f"正则断言表达式： {regular_exp_assertion}")
-
         for k, v in kwargs.items():
-            kwargs[k] = self.replace_template_str(v)
-            try:
-                kwargs[k] = json.loads(v)
-            except:
-                pass
-        logger.info("开始发送请求...")
-        before_time = time.time()
-        response = self.request(method, url, verify=False, **kwargs)
-        after_time = time.time()
-        if response.status_code == 200:
-            logger.info(f"接口请求完毕，响应：{response.text}, 时间: {after_time - before_time} s")
-            logger.info(f"响应cookie: {response.cookies}, 已保存")
-        else:
-            if self.retry_num > 2:
-                self.retry_num = 0
-                logger.info("接口请求失败，重试完毕！")
-                return response
-            logger.info(f"接口请求失败，响应code为 {response.status_code}, 进行第 {self.retry_num + 1} 次重试")
-            self.retry_num += 1
-            self.send_request(method, url, title, jsonpath_exp_save=jsonpath_exp_save,
-                              regular_exp_save=regular_exp_save,jsonpath_exp_assertion=jsonpath_exp_assertion,
-                              regular_exp_assertion=regular_exp_assertion, **kwargs)
-        """
-            处理关联参数，支持正则提取和JSONPATH提取
-        """
-        if jsonpath_exp_save:
-            for item in jsonpath_exp_save.split(";"):
-                kvs = item.split("=")
-                key = kvs[0]  # 获取关键字
-                value = kvs[1]  # 获取jsonpath
-                self.save_variable(response.text, key, jsonpath_expression=value)  # 进行JSONPATH提取并保存
-        if regular_exp_save:
-            for item in regular_exp_save.split(";"):
-                kvs = item.split("=")
-                key = kvs[0]  # 获取关键字
-                value = kvs[1]  # 获取正则表达式
-                self.save_variable(response.text, key, regular_expression=value)  # 进行正则提取并保存
-        """
-            处理断言，支持正则提取和JSONPATH提取
-        """
-        if jsonpath_exp_assertion:
-            for item in jsonpath_exp_assertion.split(";"):
-                kvs = item.split("=")
-                jsonpath_expression = kvs[0]
-                expect = kvs[1]
-                self.assert_by_expression(response.text, expect, jsonpath_expression=jsonpath_expression)
-        if regular_exp_assertion:
-            for item in regular_exp_assertion.split(";"):
-                kvs = item.split("=")
-                regular_assertion = kvs[0]
-                expect = kvs[1]
-                self.assert_by_expression(response.text, expect, regular_expression=regular_assertion)
+            if v is not None:
+                try:
+                    kwargs[k] = json.loads(self.replace_template_str(v))
+                except:
+                    logger.error(f"参数({k})格式不正确")
+        print(kwargs)
+        response = self.request(method, url, **kwargs)  # 调用接口
+        response_str = response.text
+        try:
+            response_str = json.loads(response_str)  # 转成字典
+            response_str = json.dumps(response_str, indent=4, ensure_ascii=False)
+        except:
+            logger.warning("响应JSON转换失败！保持原来格式！")
+        logger.info(f"response text: \n {response_str}")
+        # 判断是否需要关联参数, 如果有，判断是正则提取还是jsonpath提取，做对应处理，处理多参数情况存储
+        if jsonpath_exp_save or regular_exp_save:
+            if jsonpath_exp_save:
+                json_kv_list = jsonpath_exp_save.split(";")
+                for json_kv in json_kv_list:
+                    key = json_kv.split("=")[0]
+                    jsonpath_str = json_kv.split("=")[1]
+                    self.save_variables(response_str, key, jsonpath_str)
+            if regular_exp_save:
+                regular_kv_list = regular_exp_save.split(";")
+                for regular_kv in regular_kv_list:
+                    key = regular_kv.split("=")[0]
+                    regular_str = regular_kv.split("=")[1]
+                    self.save_variables(response_str, key, regular_str)
+        # 判断是否有断言，如果有就进行
+        if jsonpath_exp_assertion or regular_exp_assertion:
+            if jsonpath_exp_assertion:
+                json_kv_list = jsonpath_exp_assertion.split(";")
+                for json_kv in json_kv_list:
+                    jsonpath_str = json_kv.split("=")[0]
+                    value = json_kv.split("=")[1]
+                    self.assert_by_expression(response_str, value, jsonpath_str)
+            if regular_exp_assertion:
+                regular_kv_list = regular_exp_assertion.split(";")
+                for regular_kv in regular_kv_list:
+                    regular_str = regular_kv.split("=")[0]
+                    value = regular_kv.split("=")[1]
+                    self.assert_by_expression(response_str, value, regular_str)
         logger.info("CASE END".center(40, "*"))
-        return response
+        # 判断是否需要关联
+        # 判断是否需要断言
 
-    def replace_template_str(self, target):
-        # 正则匹配所有{{key}}，并做处理
-        EXPR = r"\{\{(.*?)\}\}"
-        keys = re.findall(EXPR, str(target))
-        if keys:
-            logger.info(f"变量池中匹配到需替换的参数: {keys}")
-        for key in keys:
-            value = self.variable_pool.get(key)
-            if not value:
-                logger.warning("变量池中未匹配到关联参数！不进行替换操作")
-                continue
-            target = target.replace('{{' + key + '}}', str(value))
-            logger.info("替换了 {{" + key + "}} 为 " + str(value))
-        return target
-
-    def save_variable(self, target, key, jsonpath_expression=None, regular_expression=None):
+    def save_variables(self, target_str, key, jsonpath_expression=None, regular_expression=None):
         """
-        存储变量到变量池
-        :param target: 目标字符串
-        :param key: 关键字
-        :param jsonpath_expression: JSONPATH表达式
-        :param regular_expression: 正则表达式
+        保存关联参数到参数池
         :return:
         """
-        match_values = jsonpath.jsonpath(json.loads(target), jsonpath_expression) if jsonpath_expression else re.findall(
-            regular_expression, target)
-        if match_values:
-            value = match_values[0]
-            self.variable_pool[key] = value
-            logger.info(f"保存了变量 {key}（{value}） 到 变量池, 当前变量池参数 {str(self.variable_pool)}")
-            return value
-        else:
-            logger.warning("未匹配到任何参数，不进行保存！")
+        target = None
+        if isinstance(target_str, str):
+            try:
+                target = json.loads(target_str)
+            except:
+                target = target_str
+        match_value = jsonpath.jsonpath(target, jsonpath_expression) if jsonpath_expression else re.findall(
+            regular_expression)
+        if match_value is False or match_value == []:
+            logger.error(f"Failed to extract variable （key: {key}）")
+            return
+        match_value = match_value[0] if match_value else match_value
+        self.variable_pool[key] = match_value  # 存储变量到变量池
+        logger.info(f"set variable ({key} = {match_value}) to variable pool")
+        logger.info(f"global variable pool: {self.variable_pool} ")
 
-    def assert_by_expression(self, target, expect, jsonpath_expression=None, regular_expression=None):
-        match_values = jsonpath.jsonpath(json.loads(target),
-                                         jsonpath_expression) if jsonpath_expression else re.findall(regular_expression,
-                                                                                                     target)
-        if match_values:
-            value = match_values[0]
-            assert value == expect, f"断言失败！实际：{value}, 期望：{expect}"
-            logger.info(f"断言成功！期望:{expect}，实际:{value}")
-        else:
-            logger.warning("未匹配到任何参数，不进行断言！")
+    def replace_template_str(self, target_str):
+        """
+        替换{{key}}位全局变量池的参数
+        :return:
+        """
+        match_values = re.findall("\{\{(.*?)\}\}", target_str)
+        if not match_values:
+            logger.info(f"Target_Str: {target_str}, This is no variable to be replaced")
+            return target_str
+        logger.info(f"The variable to be replaced is ({match_values})")
+        for key in match_values:
+            value = self.variable_pool.get(key)
+            replace_str = "{{" + key + "}}"
+            target_str = target_str.replace(replace_str, str(value))
+            logger.info(f"replace {replace_str} to {value}")
+        return target_str
+
+    def assert_by_expression(self, target_str, value, jsonpath_expression=None, regular_expression=None):
+        """
+        断言
+        :param target_str:
+        :param value:
+        :param jsonpath_expression:
+        :param regular_expression:
+        :return:
+        """
+
+        target = None
+        if isinstance(target_str, str):
+            target = json.loads(target_str)
+        match_value = jsonpath.jsonpath(target, jsonpath_expression) if jsonpath_expression else re.findall(
+            regular_expression)
+        if match_value is False or match_value == []:
+            logger.error(f"Failed to extract variable （key: {key}）")
+            return
+        match_value = match_value[0] if match_value else match_value  # 匹配到实际的值
+        try:
+            assert str(match_value) == str(value)
+            logger.info(f"断言成功! {match_value} == {value}")
+        except AssertionError:
+            logger.error(f"断言失败! {match_value} != {value}")
+            raise AssertionError(f"断言失败! {match_value} != {value}")
